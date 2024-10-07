f225a189773abe01448a512447f7d1d79e8ba687
Angel Federico <jose.angel9735@gmail.com>
2024-10-07 04:04:51 -0500

    Created Admin Component

--- hairSalon/src/app/app-routing.module.ts
+++ hairSalon/src/app/app-routing.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
       import('./salon/salon.module').then((m) => m.SalonModule),
   },
   {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
     path: '',
     redirectTo: 'salon/home',
     pathMatch: 'full',
